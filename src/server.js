d269aa809163429c67fdbe44051dbc78778915f9
avivdaniel <avivdaniel@gmail.com>
2020-05-04 21:07:04 +0300

    setup the mongoDB server

--- src/server.js
+++ src/server.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const app = express();
+const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectID;
 
-app.get('/user', (req, res) => {
-    res.send('This is the users get page!');
+//Mongo setup
+const client = new MongoClient('mongodb://localhost:27017', { useUnifiedTopology: true });
+let db;
+client.connect((err) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    db = client.db('cupon-app');
+    console.log('connection successful to the cupon-app DB');
 })
+
+app.get('/cupon', (req, res) => {
+    res.send('This is the users get page!');
+});
 
 app.listen(3000, () => {
     console.log('cupon app listning now to port 3000!');
